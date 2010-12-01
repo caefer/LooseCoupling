bca867dea2961b646e73b7092ac4377993ef4f8d
Christian Schaefer <caefer@ical.ly>
2010-12-01 01:14:46 -0800

    * dynamically add accessors that add the obj_type filter for lazy loaded relations

--- lib/Doctrine/Template/LooselyCoupled.php
+++ lib/Doctrine/Template/LooselyCoupled.php
@@ -16,6 +16,21 @@ class LooselyCoupled extends Doctrine_Template
     foreach($this->_options as $alias => $modelName)
     {
       $this->hasMany($modelName.' as '.$alias, array('local' => 'id', 'foreign' => 'obj_pk'));
+      $this->getInvoker()->hasAccessor($alias, 'getLooselyCoupledRelation');
     }
+  }
+
+  public function getLooselyCoupledRelation($load, $field)
+  {
+    $record = $this->getInvoker();
+    $table = $record->getTable();
+
+    $identifier = $record->identifier();
+    $identifier = array_shift($identifier);
+
+    return Doctrine_Query::create()
+      ->parseDqlQuery($table->getRelation($field)->getRelationDql(1))
+      ->addWhere('obj_type = ?')
+      ->execute(array($identifier, $table->getComponentName()));
   }
 }
