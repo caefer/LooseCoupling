e167ca80212f724882603a3838d08ef90b388887
Ludovic Fleury <ludovic.fleury@w3brothers.com>
2011-01-27 03:55:42 +0100

    added options to custom the definition of the LooselyCoupleable's columns (obj_type, obj_pk)

--- lib/Doctrine/Template/LooselyCoupleable.php
+++ lib/Doctrine/Template/LooselyCoupleable.php
@@ -3,11 +3,17 @@
 class LooselyCoupleable extends Doctrine_Template
 {
   protected $_objectCache = array();
+  protected $_options = array('type' => array('type'    => 'string',
+                                              'length'  => 48,
+                                              'primary' => false),
+                              'pk'   => array('length'    => 4,
+                                              'unsigned'  => true,
+                                              'primary'   => false));
 
   public function setTableDefinition()
   {
-    $this->hasColumn('obj_type', 'string', 48, array('type' => 'string', 'length' => 48));
-    $this->hasColumn('obj_pk', 'integer', 4, array('unsigned' => true));
+    $this->hasColumn('obj_type', 'string',   $this->_options['type']['length'], $this->_options['type']);
+    $this->hasColumn('obj_pk',   'integer',  $this->_options['pk']['length'],   $this->_options['pk']);
     $this->getInvoker()->unshiftFilter(new Doctrine_Record_Filter_LooselyCoupleable());
   }
 }
